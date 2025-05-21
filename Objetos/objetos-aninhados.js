55c46159b4e5cfa60b65f2bdf8f5a481e87c392a
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-21 16:41:27 -0300

    aula: atualiza conteudo sobre objetos em objetos

---
+++ Objetos/objetos-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '10293874521',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['24992381348', '2433481348']
+}
+
+estudante.endereco = {
+    rua: "Joana D'arc",
+    numero: 203,
+    bairro: 'Niteroi',
+}
+
+console.log(estudante);
