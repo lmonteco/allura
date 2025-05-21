2221488d7d3a9fc495b046a8a2e0e205284f6bf8
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-21 16:16:33 -0300

    aula: atualiza conteudo da aula sobre tipos de dados e valores

---
+++ Objetos/dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '10293874521',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['24992381348', '2433481348']
+}
+
+console.log(estudante.telefones[0]);
