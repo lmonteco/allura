df53963cff61302bc693e13ce369de1c5eb431e8
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-22 22:09:49 -0300

    chal: resolve a tarefa 2

--- Objetos/exercicio 2/infoPessoa.js
+++ Objetos/exercicio 2/infoPessoa.js
@@ -2,12 +2,18 @@ const pessoa = {
     nome: 'Lucas Monteiro',
     idade: 33,
     solteiro: false,
-    hobbies: ['Jogos de Tabuleiro', 'Tocar violão']
+    hobbies: ['Jogos de Tabuleiro', 'Tocar violão'],
+    endereco: {
+        rua: 'Rua Oitocentos e Trinta e Três',
+        cidade: 'Volta Redonda',
+        estado: 'Rio de Janeiro'
+    } //adiciona nova propriedade com valores fictícios
 };
 
 function mostrarInfoPessoa(obj) {
-    var info = `Nome: ${obj.nome}, Idade: ${obj.idade}, Solteiro: ${obj.solteiro}, Hobbies: ${obj.hobbies}`
+    var info = `Nome: ${obj.nome} / Idade: ${obj.idade} / Solteiro: ${obj.solteiro} / Hobbies: ${obj.hobbies} / Endereço: ${obj.endereco.rua}, ${obj.endereco.cidade}, ${obj.endereco.estado}` //altera o template para exibir a nova propriedade 'edereco'
     console.log(info);    
 }
 
-mostrarInfoPessoa(pessoa);
+mostrarInfoPessoa(pessoa); //exibe as informações da pessoa
+
