454601061042e3e15076dec5da58347127d82f7d
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-22 22:27:30 -0300

    chal: resolve a tarefa 3a

--- Objetos/exercicio 2/infoPessoa.js
+++ Objetos/exercicio 2/infoPessoa.js
@@ -10,10 +10,31 @@ const pessoa = {
     } //adiciona nova propriedade com valores fictícios
 };
 
+const pessoas = [{
+    nome: 'Joelson Mendes',
+    idade: 23,
+    cidade: 'Volta Redonda'
+},
+{
+    nome: 'Priscila Coutinho',
+    idade: 14,
+    cidade: 'Jundiaí'
+},
+{
+    nome: 'Mateus Proença',
+    idade: 45,
+    cidade: 'Angra dos Reis'
+}]; //cria um array de objetos 'pessoas'
+
 function mostrarInfoPessoa(obj) {
     var info = `Nome: ${obj.nome} / Idade: ${obj.idade} / Solteiro: ${obj.solteiro} / Hobbies: ${obj.hobbies} / Endereço: ${obj.endereco.rua}, ${obj.endereco.cidade}, ${obj.endereco.estado}` //altera o template para exibir a nova propriedade 'edereco'
     console.log(info);    
 }
+//mostrarInfoPessoa(pessoa); //exibe as informações da pessoa
 
-mostrarInfoPessoa(pessoa); //exibe as informações da pessoa
+//Cria função que aceita a lista pessoas como parâmetro.
+function mostrarListaPessoas(lista) {
+    lista.forEach(pessoa => console.log(`Nome: ${pessoa.nome}, Idade: ${pessoa.idade}, Cidade: ${pessoa.cidade}`))
+}
 
+mostrarListaPessoas(pessoas); //Imprime no console as informações de cada pessoa na lista.
