fd9631a9c748f4791be66413015f2ee12edb013c
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-23 09:26:21 -0300

    chal: resolve a tarefa 5

---
+++ Objetos/exercicio 2/conta-bancaria.js
@@ -0,0 +1,23 @@
+//cria o objeto contaBancaria;
+const contaBancaria = {
+    titular: 'Lucas Monteiro e Costa',
+    saldo: 15487.87,
+    depositar: function (valor) {
+        return this.saldo + valor;
+    },
+    sacar: function (valor) {
+        return this.saldo - valor
+    }
+}
+//cria o objeto cliente;
+const cliente = {
+    nome: 'Lucas Monteiro e Costa',
+    conta: contaBancaria,
+}
+//cria funcao que exibe o saldo de um cliente
+function mostrarSaldo(objeto) {
+    console.log(`Nome: ${objeto.nome}, Saldo: R$ ${objeto.conta.saldo}`);
+}
+
+mostrarSaldo(cliente);
+// console.log(cliente.conta);
