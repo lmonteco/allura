d3d2800536bb8606496e59580e62b4c8fb024f8e
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-23 10:32:52 -0300

    feat: adiciona tratativa de erro para divisão na calculadora

--- Objetos/exercicio 2/calculadora.js
+++ Objetos/exercicio 2/calculadora.js
@@ -9,7 +9,11 @@ const calculadora = {
         return a * b;
     },
     divisao: function (a, b) {
-        return a / b;
+        if (b !== 0) {
+            return a / b;
+        } else {
+            return "Não é possível realizar essa divisão."
+        }
     },
     media: function (arr) { //adiciona um novo método (média) para o objeto calculadora
         var calculo = arr.reduce((ac, valor) => ac + valor, 0) / arr.length;
@@ -21,6 +25,6 @@ const calculadora = {
 console.log(calculadora.soma(192038, 112003))
 console.log(calculadora.subtracao(29380, 12809))
 console.log(calculadora.multiplicacao(7, 23))
-console.log(calculadora.divisao(48, 3))
+console.log(calculadora.divisao(1, 2))
 //4)c chama metodo media da calculadora;
-console.log(calculadora.media([7.5, 8.5, 3.5, 6]));
+console.log(calculadora.media([7.5, 8.5, 5, 6]));
