b4eaa123209d89e2357232a80f99b6a477927c1f
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-25 00:45:40 -0300

    aula: atualiza o conteudo sobre ordenacao de objetos

---
+++ Objetos/ordena-objeto.js
@@ -0,0 +1,27 @@
+const estudantes = require('./estudantes.json');
+
+function ordenar(lista, propriedade, decrescente = false) {
+    return lista.sort((a, b) => {
+        let valorA = a[propriedade];
+        let valorB = b[propriedade];
+
+        if (typeof valorA === 'string') {
+            valorA = valorA.toUpperCase();
+        }
+        if (typeof valorB === 'string') {
+            valorB = valorB.toUpperCase();
+        }
+
+        if (valorA < valorB) {
+            return decrescente ? 1 : -1;
+        }
+        if (valorA > valorB) {
+            return decrescente ? -1 : 1;
+        }
+        return 0;
+    });
+}
+
+// Exemplo de uso para ordenar de forma decrescente pelo nome
+const ordenadoNomeDecrescente = ordenar(estudantes, "nome", false);
+console.log(ordenadoNomeDecrescente);
