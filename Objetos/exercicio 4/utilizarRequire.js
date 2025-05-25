919522e0fee79890f0dc4f42459ca7e8f21aead7
Lucas Monteiro <lmontecosta@gmail.com>
2025-05-24 21:29:22 -0300

    chal: resolve a tarefa 1

---
+++ Objetos/exercicio 4/utilizarRequire.js
@@ -0,0 +1,6 @@
+const dados = require('./dados.json') // Importar o conteúdo do arquivo dados.json.
+
+// console.log(dados); // Console do conteúdo importado
+
+console.log(dados.produtos); //acessa os produtos
+console.log(dados.usuarios); //acessa os usuarios
