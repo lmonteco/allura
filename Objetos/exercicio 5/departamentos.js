4a03ca59a6d682767195706adf040192090602ff
Lucas Monteiro <lmontecosta@gmail.com>
2025-06-09 16:36:17 -0300

    chal: resolve a tarefa 5 com muita ajuda

---
+++ Objetos/exercicio 5/departamentos.js
@@ -0,0 +1,31 @@
+const departamentos = [
+    {
+        id: 1,
+        nome: "Vendas",
+        funcionarios: [
+            { id: 101, nome: "Ana", cargo: "Vendedor" },
+            { id: 102, nome: "Carlos", cargo: "Gerente de vendas" }
+        ]
+    },
+    {
+        id: 2,
+        nome: "TI",
+        funcionarios: [
+            { id: 201, nome: "Maria", cargo: "Desenvolvedor" },
+            { id: 202, nome: "João", cargo: "Analista de sistemas" }
+        ]
+    }
+]
+
+function encontrarFuncionarioPorId(id) {
+    for (const departamento of departamentos) {
+        const funcionarioEncontrado = departamento.funcionarios.find(funcionario => funcionario.id === id);     
+        if (funcionarioEncontrado) {
+            console.log(funcionarioEncontrado);
+            return;
+        }
+    }
+    console.log('Não encontrado');    
+}
+
+encontrarFuncionarioPorId(202);
